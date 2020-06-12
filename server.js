5c9e82eb36af027e4caf07d9c7f543eef5b654d1
Alejandro Riera <ale8jandro@gmail.com>
2020-06-12 17:57:52 +0200

    Fix server js

--- server.js
+++ server.js
@@ -3,10 +3,10 @@
 var express = require('express');
 var path = require('path');
 var serveStatic = require('serve-static');
-var history = require('connect-history-api-fallback');
 
 app = express();
-app.use(history()).use(serveStatic(__dirname + '/dist'));
+//app.use(serveStatic(__dirname + '/dist'));
+app.use('/', serveStatic(path.join(__dirname, '/dist')));
 
 var port = process.env.PORT || 5000;
 app.listen(port);
